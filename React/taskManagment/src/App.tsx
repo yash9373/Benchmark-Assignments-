a12fcac98b49cf3f5036c240599951332681a288
yash9373 <kolheyashodip8@gmail.com>
2025-02-21 12:00:42 +0530

    remove streact mode

--- React/taskManagment/src/App.tsx
+++ React/taskManagment/src/App.tsx
@@ -5,13 +5,16 @@ import TaskForm from "./components/TaskForm";
 
 const App: React.FC = () => {
   return (
-    <TaskProvider>
-      <div>
-        <h1>Task Manager</h1>
-        <TaskForm />
-        <TaskList />
-      </div>
-    </TaskProvider>
+    console.log("App"),
+    (
+      <TaskProvider>
+        <div>
+          <h1>Task Manager</h1>
+          <TaskForm />
+          <TaskList />
+        </div>
+      </TaskProvider>
+    )
   );
 };
 
