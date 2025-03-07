6b56318221dcb581ff78bf21581eeb11ddf8144c
Yashodip Kolhe <101787484+yash9373@users.noreply.github.com>
2025-03-07 12:51:48 +0530

    Revert "Mini project"

--- Mini Project/User And Company Management Dash/src/pages/companies.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import CompaniesList from "@/componets/compniesList";
-export default function Companies() {
-  return (
-    <div>
-      <CompaniesList />
-    </div>
-  );
-}
--- Mini Project/User And Company Management Dash/src/pages/posts.tsx
+++
@@ -1,8 +0,0 @@
-import PostList from "@/componets/postList";
-import { usePost } from "@/servises/postServises";
-
-export default function Posts() {
-  const post = usePost();
-  console.log("Posts data:", post.posts);
-  return <PostList posts={post.posts} />;
-}
